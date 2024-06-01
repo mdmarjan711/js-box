776d4489609c80e8da25b6998f18e3f95114be9e
Md Marjan <mdmarjan711@gmail.com>
2024-06-01 08:57:33 +0600

    code push

---
+++ chapter1/practiceProblem1.js
@@ -0,0 +1,25 @@
+//* problem 1
+var momGaveMoney = 1000
+var applePrice = 400
+var orangePrice = 300
+var returnMoney = 1000-(400+300)
+console.log(returnMoney);
+
+//* problem 2
+var math = 75.25
+var biology = 65
+var chemistry = 80
+var physics = 35.45
+var bangla = 99.50
+var average = ((math+biology+chemistry+physics+bangla)/5)
+console.log(average.toFixed(2));
+
+//* problem 3
+var a = 'I am gong to be'
+var b = 'an awesome web developer'
+console.log(a+' '+b);
+
+//* problem 4
+// output remainder
+var c = 119
+console.log(c%5);
