5cde96282be8d617d3b37248608bb9f0fb748e7a
Md Marjan <mdmarjan711@gmail.com>
2024-06-01 11:27:20 +0600

    code push

---
+++ chapter1/typeof.js
@@ -0,0 +1,34 @@
+var a = "Bangladesh"
+console.log(typeof(a));
+
+var b = 2133
+console.log(typeof(b));
+
+var c = true
+console.log(typeof(c));
+
+var d = false
+console.log(typeof(d));
+
+var e = null
+console.log(typeof(e));
+
+var f 
+console.log(typeof(f));
+
+var g = 523.11
+console.log(typeof(g));
+
+//* toFixed
+var h = 0.1
+var i = 0.2
+var j = (h+i)
+j = j.toFixed(1)
+j = parseFloat(j) 
+console.log(j);
+
+//*reminder
+// + - * / %
+var k = 19
+var l = 7
+console.log(k%l);
