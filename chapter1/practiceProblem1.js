4b8660342d4762c9decc28cf1fb60ad63f8fbc63
Md Marjan <mdmarjan711@gmail.com>
2024-06-01 10:48:02 +0600

    code push

--- chapter1/practiceProblem1.js
+++ chapter1/practiceProblem1.js
@@ -7,7 +7,7 @@ console.log(returnMoney);
 
 //* problem 2
 var math = 75.25
-var biology = 65
+var biology = 65 
 var chemistry = 80
 var physics = 35.45
 var bangla = 99.50
---
+++ chapter1/typeconversition.js
@@ -0,0 +1,23 @@
+var a = '1000'
+console.log(parseInt(a));
+
+var b = 255.23
+console.log(parseInt(b));
+
+var c = '4111.55'
+console.log(parseFloat(c));
+
+var d = '555'
+console.log(Number(d));
+
+var e = true
+console.log(Boolean(e)); //true
+
+var f = 0
+console.log(Boolean(f));  //false
+
+var g = 850
+console.log(g.toString()); //string
+
+var h = 120
+console.log(String(h));
