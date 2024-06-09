045b5725d6cbbb9e8f455ee87dc6e3d20b21613c
Md Marjan <mdmarjan711@gmail.com>
2024-06-09 14:20:54 +0600

    code push

---
+++ chapter1/math.js
@@ -0,0 +1,17 @@
+//js builtin math funtion
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.max(200,500,300));
+console.log(Math.min(50,600,5250));
+console.log(Math.pow(3,5));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(9));
+
+var a = 4.589
+console.log(Math.abs(a));
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(Math.round(a));
+
+console.log(Math.round(Math.random()*11+1));
