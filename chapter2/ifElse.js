4de2efbf3bac76f0e0fe814b2e4a63fea992f848
Md Marjan <mdmarjan711@gmail.com>
2024-06-11 19:15:44 +0600

    code push

--- chapter2/ifElse.js
+++ chapter2/ifElse.js
@@ -7,3 +7,21 @@ if(a>85){
     console.log("a is small");
 }
 
+var marks=59
+if(marks>90) {
+    console.log('AA');
+}else if(marks>80 && marks<=90){
+    console.log('AB');
+}else if(marks>70 && marks<=80){
+    console.log('BB');
+}else if(marks>60 && marks<=70){
+    console.log('BC');
+}else if(marks>50 && marks<=60){
+    console.log('CC');
+}else if(marks>40 && marks<=50){
+    console.log('CD');
+}else if(marks>30 && marks<=40){
+    console.log('DD');
+}else if(marks<=30){
+    console.log('FF');
+}
