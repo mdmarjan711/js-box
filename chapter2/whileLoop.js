d742435cf599447649fbe45015b5bbb0cb77b9e8
Md Marjan <mdmarjan711@gmail.com>
2024-06-16 19:43:38 +0600

    code push

---
+++ chapter2/whileLoop.js
@@ -0,0 +1,16 @@
+var i = 1
+while (i<=10) {
+    console.log(i,'Bangladesh');
+    i++
+}
+
+var isRuning = true
+while (isRuning) {
+    var num = Math.floor(Math.random()*10+1)
+    if (num==9) {
+        console.log('winer winer');
+        isRuning = false
+    }else{
+        console.log('you have got '+num);
+    }
+}
