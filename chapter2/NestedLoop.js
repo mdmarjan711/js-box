334676b99e311d4d279ac92ce942f8e30c9ce8de
md marjan <mdmarjan711@gmail.com>
2024-06-25 15:20:33 +0600

    code push

---
+++ chapter2/NestedLoop.js
@@ -0,0 +1,19 @@
+var i = 5
+for(var a = 1; a<=i; a++){
+    var c = ''
+    for( var b = 1; b<=i; b++){
+        c+= '*'+' '
+    }
+        console.log(c);
+}
+
+var e = 5
+for( var w = 1; w<=e; w++){
+    var x = ''
+    for(var u = 1; u<=w; u++){
+       x+= u+' '
+       // x = x+u+' '
+    }
+     console.log(x);
+}
+
