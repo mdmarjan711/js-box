ba6aa1ab28162d0bc198f3c0e1f4c57a9794cf5e
md marjan <mdmarjan711@gmail.com>
2024-06-27 12:35:14 +0600

    code push

---
+++ chapter2/break&continue.js
@@ -0,0 +1,37 @@
+// Breake statement
+
+while(true){
+    var d = Math.floor(Math.random()*10+1)
+    if(d == 9){
+        console.log('winer winer');
+        break
+    }else{
+        console.log('i got this number',d);
+    }
+}
+
+for( var a = 1; a<10; a++){
+    if(a == 7){
+        break
+    }
+    console.log(a);
+}
+
+// continue statement
+
+for( var a = 1; a<=10; a++){
+        if(a == 3 || a == 7){
+            continue
+        }
+        console.log(a);
+    }
+
+for(;;){
+    var d = Math.floor(Math.random()*10+1)
+    if(d == 9){
+        console.log('win win');
+        break
+    }else{
+        console.log('i got',d);
+    }
+}
